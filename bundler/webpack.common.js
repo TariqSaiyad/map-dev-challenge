57b69c9083d47b2df5bdc3f7bcc9690eb170ae48
Tariq Saiyad <tariq.saiyad@education.govt.nz>
2021-05-27 20:07:04 +0000

    test stuff

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -1,88 +1,74 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+const themeDir = '/home/ubuntu/map-dev-challenge';
+const inputDir = `${themeDir}/src`;
+const outputDir =  `../dist`;
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/script.js'),
-    output:
-    {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
-    },
-    devtool: 'source-map',
-    plugins:
-    [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, '../static') }
-            ]
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
-            minify: true
-        }),
-        new MiniCSSExtractPlugin()
-    ],
-    module:
-    {
-        rules:
-        [
-            // HTML
-            {
-                test: /\.(html)$/,
-                use: ['html-loader']
-            },
-
-            // JS
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
-                ]
-            },
-
-            // CSS
-            {
-                test: /\.css$/,
-                use:
-                [
-                    MiniCSSExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-
-            // Images
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                use:
-                [
-                    {
-                        loader: 'file-loader',
-                        options:
-                        {
-                            outputPath: 'assets/images/'
-                        }
-                    }
-                ]
-            },
-
-            // Fonts
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                use:
-                [
-                    {
-                        loader: 'file-loader',
-                        options:
-                        {
-                            outputPath: 'assets/fonts/'
-                        }
-                    }
-                ]
-            }
-        ]
-    }
+  mode: 'development',
+  devtool: 'source-map',
+  entry: {
+    'app': [`${themeDir}/src/script.js`],
+  },
+  output: {
+    path: path.resolve(__dirname, outputDir),
+    filename: `[name].js`,
+  },
+  module: {
+    rules: [
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ["@babel/preset-env", {	targets: { ie: "11" } }],
+            ],
+          }
+        }
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+                  // CSS
+                  {
+                    test: /\.css$/,
+                    use:
+                    [
+                        MiniCSSExtractPlugin.loader,
+                        'css-loader'
+                    ]
+                },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader',
+      },
+    ]
+  },
+  resolve: {
+   
+    extensions: ['*', '.js', '.vue', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, `${themeDir}/src/index.html`),
+        minify: true
+    }),
+    new MiniCSSExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: `${themeDir}/assets/images`, to: 'images' },
+      ],
+    }),
+  ],
 }
--- src/script.js
+++ src/script.js
@@ -2,103 +2,261 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import ShaderTerrain from './terrain-shader.js'
 
-// Debug
-const gui = new dat.GUI()
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
 
-// Scene
-const scene = new THREE.Scene()
+import Stats from 'three/examples/jsm/libs/stats.module.js';
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+import { ImprovedNoise } from 'three/examples/jsm/math/ImprovedNoise.js';
 
-// Materials
+let container, stats;
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+let camera, controls, scene, renderer;
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+let mesh, texture;
 
-// Lights
+var img = new Image();
+img.src = 'images/heightmap.jpg';
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+let worldWidth = img.width;
+let worldDepth = img.height;
+let worldHalfDepth = worldDepth/2
+let worldHalfWidth=worldWidth/2
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+let helper;
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+init();
+animate();
+
+function init() {
+
+    container = document.getElementById( 'container' );
+    container.innerHTML = '';
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xbfd1e5 );
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 10, 20000 );
+
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.minDistance = 1000;
+    controls.maxDistance = 10000;
+    controls.maxPolarAngle = Math.PI / 2;
+
+    //
+
+    const data = generateHeight(img);
+    controls.target.y = data[ worldHalfWidth + worldHalfDepth * worldWidth ] + 500;
+    camera.position.y = controls.target.y + 2000;
+    camera.position.x = 2000;
+    controls.update();
+
+    const geometry = new THREE.PlaneGeometry( 7500, 7500, worldWidth - 1, worldDepth - 1 );
+    geometry.rotateX( - Math.PI / 2 );
+
+    const vertices = geometry.attributes.position.array;
+
+    for ( let i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3 ) {
+
+        vertices[ j + 1 ] = data[ i ] * 10;
+
+    }
+
+    geometry.computeFaceNormals(); // needed for helper
+
+    //
+
+    texture = new THREE.CanvasTexture( generateTexture( data, worldWidth, worldDepth ) );
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.ClampToEdgeWrapping;
+
+    mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture } ) );
+    scene.add( mesh );
+
+    const geometryHelper = new THREE.ConeGeometry( 20, 100, 3 );
+    geometryHelper.translate( 0, 50, 0 );
+    geometryHelper.rotateX( Math.PI / 2 );
+    helper = new THREE.Mesh( geometryHelper, new THREE.MeshNormalMaterial() );
+    scene.add( helper );
+
+    container.addEventListener( 'pointermove', onPointerMove );
+
+    stats = new Stats();
+    container.appendChild( stats.dom );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize );
+
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+function onWindowResize() {
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+    renderer.setSize( window.innerWidth, window.innerHeight );
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
 }
 
-tick()
+function generateHeight(img) {
+let scale = 1;
+var c = document.createElement( 'img' );
+        c.width = img.width;
+        c.height = img.height;
+        var context = c.getContext( '2d' );
+
+        var size = img.width * img.height;
+        var data = new Float32Array( size );
+
+        context.drawImage(img,0,0);
+
+        for ( var i = 0; i < size; i ++ ) {
+            data[i] = 0
+        }
+
+        var imgd = context.getImageData(0, 0, img.width, img.height);
+        var pix = imgd.data;
+
+        var j=0;
+        for (var i = 0; i<pix.length; i +=4) {
+            var all = pix[i]+pix[i+1]+pix[i+2];
+            data[j++] = all/(12*scale);
+        }
+
+
+    // const size = width * height, data = new Uint8Array( size ),
+    //     perlin = new ImprovedNoise(), z = Math.random() * 100;
+
+    // let quality = 20;
+
+    // // for ( let j = 0; j < 4; j ++ ) {
+
+    //     for ( let i = 0; i < size; i ++ ) {
+
+    //         const x = i % width, y = ~ ~ ( i / width );
+    //         data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * quality * 1.75 );
+
+    //     }
+
+        // quality *= 5;
+
+    // }
+
+    return data;
+
+}
+
+function generateTexture( data, width, height ) {
+
+    // bake lighting into texture
+
+    let context, image, imageData, shade;
+
+    const vector3 = new THREE.Vector3( 0, 0, 0 );
+
+    const sun = new THREE.Vector3( 1, 1, 1 );
+    sun.normalize();
+
+    const canvas = document.createElement( 'canvas' );
+    canvas.width = width;
+    canvas.height = height;
+
+    context = canvas.getContext( '2d' );
+    context.fillStyle = '#000';
+    context.fillRect( 0, 0, width, height );
+
+    image = context.getImageData( 0, 0, canvas.width, canvas.height );
+    imageData = image.data;
+
+    for ( let i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
+
+        vector3.x = data[ j - 2 ] - data[ j + 2 ];
+        vector3.y = 2;
+        vector3.z = data[ j - width * 2 ] - data[ j + width * 2 ];
+        vector3.normalize();
+
+        shade = vector3.dot( sun );
+
+        imageData[ i ] = ( 96 + shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
+        imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+        imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+
+    }
+
+    context.putImageData( image, 0, 0 );
+
+    // Scaled 4x
+
+    const canvasScaled = document.createElement( 'canvas' );
+    canvasScaled.width = width * 4;
+    canvasScaled.height = height * 4;
+
+    context = canvasScaled.getContext( '2d' );
+    context.scale( 4, 4 );
+    context.drawImage( canvas, 0, 0 );
+
+    image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
+    imageData = image.data;
+
+    for ( let i = 0, l = imageData.length; i < l; i += 4 ) {
+
+        const v = ~ ~ ( Math.random() * 5 );
+
+        imageData[ i ] += v;
+        imageData[ i + 1 ] += v;
+        imageData[ i + 2 ] += v;
+
+    }
+
+    context.putImageData( image, 0, 0 );
+
+    return canvasScaled;
+
+}
+
+//
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    render();
+    stats.update();
+
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+
+}
+
+function onPointerMove( event ) {
+
+    pointer.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+    pointer.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+    raycaster.setFromCamera( pointer, camera );
+
+    // See if the ray from the camera into the world hits one of our meshes
+    const intersects = raycaster.intersectObject( mesh );
+
+    // Toggle rotation bool for meshes that we clicked
+    if ( intersects.length > 0 ) {
+
+        helper.position.set( 0, 0, 0 );
+        helper.lookAt( intersects[ 0 ].face.normal );
+
+        helper.position.copy( intersects[ 0 ].point );
+
+    }
+
+}
